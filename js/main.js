37069ac6009cacee9ef3f92a3674c13366d85943
regine-tiu <regine.d.tiu@gmail.com>
2021-06-29 16:16:58 +0800

    added exhibit zoom in jquery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(document).ready(() => {
+    /*const $curatorSection = $('.curator-text');
+    const $exhibitSection = $('.exhibition');
+    const $landSection = $('.land-acknowledgement');
+    const $thanksSection = $('.special-thanks');
+
+    $curatorSection.on('scroll', () => {
+        $curatorSection.fadeIn(4000); 
+    }) */
 
 
+    $('.box').on('mouseenter', event => { 
+        $(event.currentTarget).addClass('photo-active');
+    }).on('mouseleave', event => {
+        $(event.currentTarget).removeClass('photo-active');
+    });
 
 })
