668579c3bd26629c2ad75fbb7cf86cc34d8ee44d
regine-tiu <regine.d.tiu@gmail.com>
2021-07-01 18:31:18 +0800

    added js color randomizer

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ $(document).ready(() => {
     }) */
 
 
+    /*----- Photo Gallery Zoom in Hover --------*/
     $('.box').on('mouseenter', event => { 
         $(event.currentTarget).addClass('photo-active');
     }).on('mouseleave', event => {
@@ -16,3 +17,35 @@ $(document).ready(() => {
     });
 
 })
+
+/*----- Title Color Randomizer --------*/
+let targetTitle = document.getElementById('target-title');
+
+function colorValue() {
+  return Math.floor(Math.random() * 256);
+}
+
+function backgroundColorChange(event){
+  let randomColor = 'rgb(' + colorValue() + ',' + colorValue() + ',' + colorValue() + ')';
+  event.target.style.backgroundColor = randomColor;
+}
+
+targetTitle.addEventListener("wheel", backgroundColorChange);
+
+
+/*----- Subheader Color Randomizer --------*/
+const subHeader1 = document.getElementById('sub-header1');
+const subHeader2 = document.getElementById('sub-header2');
+const subHeader3 = document.getElementById('sub-header3');
+const subHeader4 = document.getElementById('sub-header4');
+
+function fontColorChange(event){
+    let randomColor = 'rgb(' + colorValue() + ',' + colorValue() + ',' + colorValue() + ')';
+    event.target.style.color = randomColor;
+  }
+
+subHeader1.addEventListener('wheel',fontColorChange);  
+subHeader2.addEventListener('wheel',fontColorChange);  
+subHeader3.addEventListener('wheel',fontColorChange);  
+subHeader4.addEventListener('wheel',fontColorChange);  
+  
