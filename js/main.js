a94919fb3fb061bb7bd08425a3f9a72c485089ca
regine-tiu <regine.d.tiu@gmail.com>
2021-07-15 21:01:18 -0400

    davis html complete

--- js/main.js
+++ js/main.js
@@ -32,22 +32,9 @@ function backgroundColorChange(event){
 
 targetTitle.addEventListener("wheel", backgroundColorChange);
 
-
-/*----- Subheader Color Randomizer --------*/
-const subHeader1 = document.getElementById('sub-header1');
-const subHeader2 = document.getElementById('sub-header2');
-const subHeader3 = document.getElementById('sub-header3');
-const subHeader4 = document.getElementById('sub-header4');
-
-
 function fontColorChange(event){
     let randomColor = 'rgb(' + colorValue() + ',' + colorValue() + ',' + colorValue() + ')';
     event.target.style.color = randomColor;
   }
-
-subHeader1.addEventListener('wheel',fontColorChange);  
-subHeader2.addEventListener('wheel',fontColorChange);  
-subHeader3.addEventListener('wheel',fontColorChange);  
-subHeader4.addEventListener('wheel',fontColorChange);  
  
   
